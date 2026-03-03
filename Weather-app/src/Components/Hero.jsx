b1ee6b72842fd7ef593b8f93871fa5360a2a4c76
BSammel <160643584+BSammel@users.noreply.github.com>
2026-03-03 23:36:20 +0000

    Remove API key from weather request
    Removed hardcoded API key from axios request.

--- Weather-app/src/Components/Hero.jsx
+++ Weather-app/src/Components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
         axios.get('https://api.openweathermap.org/data/3.0/onecall',{
           params: {
               q: input,
-              appid: 'e88191f36187567a4734733ea7cf8458',
+              appid: '',
               units: 'metric'
           }
         }).then(res => {
