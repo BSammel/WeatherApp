c113410d355ae25421b41a33af2c2689a0c4405d
BSammel <bempongsammel@gmail.com>
2026-03-07 15:05:34 +0000

    Added new features

--- Weather-app/src/Components/Hero.jsx
+++ Weather-app/src/Components/Hero.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { section } from 'framer-motion/client'
 import { Search, Cloudy, Droplet, Wind, Rocket, Sun} from 'lucide-react'
-import React, { useState } from 'react'
+import { useState } from 'react'
+import {Loader} from 'lucide-react'
 
 
 const Hero = () => {
@@ -11,26 +12,40 @@ const Hero = () => {
      const [weather, setWeather] = useState({
       loading: false,
       data: {},
-      err: false
+      error: false
      })
 
      const search = (event) =>{
-      if(event.key === 'Enter'){
+      if(event.key === "Enter"){
         setInput('')
         setWeather({...weather, loading: true})
-        axios.get('https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}',{
+        axios.get('https://api.openweathermap.org/data/2.5/weather?',{
           params: {
               q: input,
-              appid: '',
+              appid: 'e88191f36187567a4734733ea7cf8458',
               units: 'metric'
           }
         }).then(res => {
           console.log(res)
+          setWeather({data: res.data, loading: false, error: false})
         }).catch(err =>{
-          setWeather({...weather, data: {}, err: true})
+          setWeather({...weather, data: {}, error: true})
         })
       }
      }
+
+     const toDate = () => {
+      const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+      const currentDate = new Date();
+     const day = currentDate.getDate();
+     const month = months[currentDate.getMonth()];
+     const year = currentDate.getFullYear();
+     const date = `${day} ${month}, ${year}`
+     return date;
+     }
+
+     
 
   return (
     <section>
@@ -49,15 +64,27 @@ const Hero = () => {
       <button className="absolute right-2 top-2 bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 transition-colors">Search</button>
     </div>
 
-    <div className=' max-w-lg mx-auto mt-10 h-70 shadow-lg backdrop-blur-sm border border-blue-700 rounded-lg'>
+    <div className=' max-w-lg mx-auto mt-10 h-85 shadow-lg backdrop-blur-sm border border-blue-700 rounded-lg'>
         <div className='space-x-1 flex items-center justify-center'>
-        <h1 className='text-6xl text-yellow-300 font-bold pt-2 flex items-center justify-center'>28</h1>
-        <p className='text-3xl text-yellow-300 font-bold flex items-center justify-center pt-1/2'>{cel}C</p>
+          {
+            weather.loading && (<Loader className='animate-spin text-gray-500 w-24 h-24' />)
+          }
+          {
+            weather.error && (<p className='text-red-500 text-sm'>City not found. Please try again.</p>)
+          }
+        <h1 className='text-6xl text-yellow-300 font-bold pt-2 flex items-center justify-center'>{weather.data.main?.temp.toFixed(0)}{cel}C</h1>
+        <p className='text-3xl text-yellow-300 font-bold flex items-center justify-center pt-1/2'></p>
         </div>
-        <p className='text-yellow-300 text-4xl flex items-center justify-center pt-2'>Accra, GH</p>
+        <div className='flex items-ceneter justify-center space-x-3'>
+        <p className='text-yellow-300 text-4xl flex items-center justify-center pt-2'>{weather.data.name}</p>
+        <span className='text-yellow-300 text-4xl flex items-center justify-center pt-2'>{weather.data.sys?.country}</span>
+        </div>
         <div className='flex items-center justify-center pt-5'>
-        <Cloudy className='text-gray-300 w-24 h-12' />
-        <p className='text-yellow-300 text-xl'>cloudy</p>
+          <span className='text-blue-800 text-sm'>{toDate()}</span>
+        </div>
+        <div className='flex items-center justify-center space-x-3 pt-2'>
+        <img src={`https://openweathermap.org/img/wn/${weather.data.weather?.[0].icon}@2x.png`} alt={weather.data.weather?.[0].description} />
+        <p className='text-yellow-300 text-xl'>{weather.data.weather?.[0].description}</p>
         </div>
          <hr style={{
             background: 'lightgray',
@@ -69,22 +96,22 @@ const Hero = () => {
          <div className='flex items-center justify-center pt-2 space-x-4'>
             <div className='flex items-center justify-center space-x-2'>
             <Droplet className='text-blue-700 w-6 h-6' />
-            <p className='text-gray-800 text-sm pl-1/2'>Humidity 75%</p>
+            <p className='text-gray-800 text-sm pl-1/2'>Humidity {weather.data.main?.humidity}%</p>
             </div>
             <div className='flex items-center justify-center space-x-2'>
             <Wind className='text-blue-600 w-6 h-6' />
-            <p className='text-gray-800 text-sm pl-1/2'>Wind 12 km/h</p>
+            <p className='text-gray-800 text-sm pl-1/2'>Wind {weather.data.wind?.speed.toFixed(1)} m/s</p>
             </div>
             <div className='flex items-center justify-center space-x-2'>
             <Rocket className='text-blue-600 w-6 h-6' />
-            <p className='text-gray-800 text-sm pl-1/2'>Pressure 1013 hPa</p>
+            <p className='text-gray-800 text-sm pl-1/2'>Pressure {weather.data.main?.pressure} hPa</p>
             </div>
             <hr/>
                 
          </div>
          <div className='flex items-center justify-center space-x-2'>
-         <Sun className='text-yellow-700 w-6 h-6 pt-2' />
-         <div className='text-gray-800 flex items-center justify-center text-sm pt-3'>Feels like 30°C</div>
+         <img className='w-8 h-8' src={`https://openweathermap.org/img/wn/${weather.data.weather?.[0].icon}@2x.png`} alt={weather.data.weather?.[0].description} />
+         <div className='text-gray-800 flex items-center justify-center text-sm pt-3'>Feels like {weather.data.main?.feels_like.toFixed(0)}°C</div>
          </div>
     </div>
     </section>
