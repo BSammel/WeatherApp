3a9be8b543dcd51c85e861d968d9974b3074be19
BSammel <bempongsammel@gmail.com>
2026-03-07 15:14:53 +0000

    Added new features

--- Weather-app/src/Components/Hero.jsx
+++ Weather-app/src/Components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
         axios.get('https://api.openweathermap.org/data/2.5/weather?',{
           params: {
               q: input,
-              appid: 'e88191f36187567a4734733ea7cf8458',
+              appid: import.meta.env.VITE_APP_ID,
               units: 'metric'
           }
         }).then(res => {
